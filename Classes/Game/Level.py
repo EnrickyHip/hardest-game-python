9252b85d70332b0eac89cab30016e666bd90431b
Enricky Hipólito <enricky.hipolito@gmail.com>
2022-03-16 16:45:07 -0300

    Refactoring Level methods
    Creatin the methods to Addsprites, decrease player alpha (decreaseAlpha) and methods to test especific collisions for walls, enemies and coins

--- Classes/Game/Level.py
+++ Classes/Game/Level.py
@@ -15,18 +15,25 @@ class Level:
         self.active_sprite_list = pygame.sprite.Group()
 
         self.info = Rectangle(800, 36, 0, 0)
-        self.active_sprite_list.add(self.info)
-
         self.background = Background(self.backgorund, self.screen)
         self.player = Player(self, spawnx, spawny)
-        self.active_sprite_list.add(self.player)
 
+        self.total_coins = len(coins)
+        self.gotten_coins = self.total_coins
+
+        #creating sprite groups
         self.walls = pygame.sprite.Group()
         self.rectangles = pygame.sprite.Group()
         self.enemies = pygame.sprite.Group()
         self.coins = pygame.sprite.Group()
-        self.total_coins = len(coins)
-        self.gotten_coins = self.total_coins
+
+        self.addSprites(walls, enemies, coins)
+
+
+    def addSprites(self, walls, enemies, coins): #add sprites to respective group
+
+        self.active_sprite_list.add(self.info)
+        self.active_sprite_list.add(self.player)
 
         for wall in walls:
             self.walls.add(wall)
@@ -39,6 +46,7 @@ class Level:
             self.coins.add(coin)
             self.active_sprite_list.add(coin)
 
+
     def draw(self):
         self.background.draw()
         self.player.movement(self)
@@ -50,13 +58,24 @@ class Level:
         if not self.player.get_alive():
             self.die()
 
+
     def test_collision(self, x, y):
+
         if self.player.get_alive():
             self.player.set_rect_x(self.player.get_rect_x() + x)
             self.player.set_rect_y(self.player.get_rect_y() + y)
+
         self.player.get_image().set_alpha(self.player.get_alpha())
 
+        self.wall_collision(x, y)
+        self.enemy_collision()
+        self.coin_collision()
+
+
+    def wall_collision(self, x, y):
+
         wall_list = pygame.sprite.spritecollide(self.player, self.walls, False) # wall collisions
+
         for wall in wall_list:
             if x > 0:
                 self.player.get_rect().right = wall.rect.left
@@ -67,7 +86,9 @@ class Level:
             elif y > 0:
                 self.player.get_rect().bottom = wall.rect.top
 
-        enemy_list = pygame.sprite.spritecollide(self.player, self.enemies, False) # enemy collisions
+    def enemy_collision(self):
+        enemy_list = pygame.sprite.spritecollide(self.player, self.enemies, False)  # enemy collisions
+
         if enemy_list:
             self.player.set_alive(False)
 
@@ -75,23 +96,32 @@ class Level:
             self.game.actual_level += 1
             self.game.active_sprite_list = pygame.sprite.Group()
 
-        coin_list = pygame.sprite.spritecollide(self.player, self.coins, False) # coin collisions
+    def coin_collision(self):
+        coin_list = pygame.sprite.spritecollide(self.player, self.coins, False) # coin collsions
+
         for coin in coin_list:
             if coin in self.active_sprite_list:
                 self.active_sprite_list.remove(coin)
                 self.gotten_coins -= 1
 
+
     def die(self):
-        if self.player.get_alpha() > 0:
-            self.player.set_alpha(self.player.get_alpha() - 7)
-        else:
-            self.player.set_alpha(255)
+        if self.decreaseAlpha():
             self.player.set_alive(True)
             self.player.set_rect_x(self.spawnx)
             self.player.set_rect_y(self.spawny)
+
             self.game.deaths += 1
 
             for coin in self.coins:
                 self.active_sprite_list.add(coin)
                 self.gotten_coins = self.total_coins
+
+    def decreaseAlpha(self):
+        if self.player.get_alpha() > 0:
+            self.player.set_alpha(self.player.get_alpha() - 7)
+        else:
+            self.player.set_alpha(255)
+            return True
+
 
